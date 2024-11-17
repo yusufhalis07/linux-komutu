54a68d1b6ebf6fb4cf00e648da21c37e184854d5
yusufhalis07 <yusufhalis250@gmail.com>
2024-11-17 16:10:45 +0300

    Delete linux komutları directory

--- linux komutları/listeler.python.py
+++
@@ -1,8 +0,0 @@
-liste = [[0,1,2] , [4,5,6,7,8,9,10]]
-print(liste[0][2])#burada 0 ıncı listenin ikinci elemanını gösterir
-
-liste1=[0,1,2,3,4,5,6]
-liste2=["a","b","c","d","f"]
-for rakam in liste1:
- for harf in liste2:
-   print(harf,rakam)
